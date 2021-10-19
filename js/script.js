8c6ad5ebcf12ab57fb309d7855cbfe2edb511cfe
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-10-19 16:24:26 +1030

    added function to create lists for emotions

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ var tvList = [];
 var movieObject = [];
 var tvObject = [];
 
+var happy = [];
+var sad = [];
+var angry = [];
+var anxious = [];
+var inLove = [];
+
+
 //creates an array of movies with IMDb-API
 function getMovieListAPI(){
     fetch("https://imdb-api.com/en/API/Top250Movies/k_7sn98acw")
@@ -72,7 +79,121 @@ function getTitleForMoviesAndShows(){
         })
     }
 }
+//seperate movies
 
+//function to get a list of happy movies
+function getHappy(){
+    //array for genre options
+    var emotionHappy = ["Adventure", "Animation", "Comedy", "Family", "Fantasy", "Gameshow", "Musical"]
+    var movieAlreadyAdded = false;
+    //loop to go through the genre
+    for(i=0;i<emotionHappy.length;i++){
+        //loops through movies
+        for(j=0;j<movieObject.length;j++){
+            //checks if movie has already been added or not
+            if(movieObject[j].genre.search(emotionHappy[i]) >= 0){
+                for(k=0;k<happy.length;k++){
+                    if(happy[k] === movieObject[j]){
+                        movieAlreadyAdded = true;
+                    }
+                }
+                //pushes movie to the emotion array
+                if(!movieAlreadyAdded){
+                    happy.push(movieObject[j])
+                }else{
+                    movieAlreadyAdded = false;
+                }
+            }
+        }
+    }
+}
+//gets list of sad movies
+function getSad(){
+    var emotionSad = ["Documentary", "Drama", "History", "Romance", "War"]
+    var movieAlreadyAdded = false;
+    for(i=0;i<emotionSad.length;i++){
+        for(j=0;j<movieObject.length;j++){
+            if(movieObject[j].genre.search(emotionSad[i]) >= 0){
+                for(k=0;k<sad.length;k++){
+                    if(sad[k] === movieObject[j]){
+                        movieAlreadyAdded = true;
+                    }
+                }
+                if(!movieAlreadyAdded){
+                    sad.push(movieObject[j])
+                }else{
+                    movieAlreadyAdded = false;
+                }
+            }
+        }
+    }
+}
+//gets list of Angry Movies
+function getAngry(){
+    var emotionAngry = ["Action", "Crime", "Documentary", "History", "Sci-Fi", "Sport", "War", "Western"]
+    var movieAlreadyAdded = false;
+    for(i=0;i<emotionAngry.length;i++){
+        for(j=0;j<movieObject.length;j++){
+            if(movieObject[j].genre.search(emotionAngry[i]) >= 0){
+                for(k=0;k<angry.length;k++){
+                    if(angry[k] === movieObject[j]){
+                        movieAlreadyAdded = true;
+                    }
+                }
+                if(!movieAlreadyAdded){
+                    angry.push(movieObject[j])
+                }else{
+                    movieAlreadyAdded = false;
+                }
+            }
+        }
+    }
+}
+//get list of Anxious Movies
+function getAnxious(){
+    var emotionAnxious = ["Crime", "Documentary", "Film-Noir", "Horror", "Music", "Mystery", "Thriller"]
+    var movieAlreadyAdded = false;
+    for(i=0;i<emotionAnxious.length;i++){
+        for(j=0;j<movieObject.length;j++){
+            if(movieObject[j].genre.search(emotionAnxious[i]) >= 0){
+                for(k=0;k<anxious.length;k++){
+                    if(anxious[k] === movieObject[j]){
+                        movieAlreadyAdded = true;
+                    }
+                }
+                if(!movieAlreadyAdded){
+                    anxious.push(movieObject[j])
+                }else{
+                    movieAlreadyAdded = false;
+                }
+            }
+        }
+    }
+}
+//gets list of loving movies
+function getInLove(){
+    var emotionInLove = ["Adventure", "Animation", "Drama", "Family", "Fantasy", "Romance"]
+    var movieAlreadyAdded = false;
+    for(i=0;i<emotionInLove.length;i++){
+        for(j=0;j<movieObject.length;j++){
+            if(movieObject[j].genre.search(emotionInLove[i]) >= 0){
+                for(k=0;k<inLove.length;k++){
+                    if(inLove[k] === movieObject[j]){
+                        movieAlreadyAdded = true;
+                    }
+                }
+                if(!movieAlreadyAdded){
+                    inLove.push(movieObject[j])
+                }else{
+                    movieAlreadyAdded = false;
+                }
+            }
+        }
+    }
+}
+//end of get emotion functions
+
+//code for materialize carousel
 $('.carousel').carousel({
     fullWidth: true,
     // indicators: true,
